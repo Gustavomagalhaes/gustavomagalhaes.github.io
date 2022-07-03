722d87d8603232e42d452fec6147e3fe45b5d3f6
Gustavo Magalhães Pereira <itsgustavopereira@gmail.com>
2022-07-03 01:02:59 +0000

    slides

--- layseluiz/assets/slide.js
+++ layseluiz/assets/slide.js
@@ -1,9 +1,12 @@
-var slides = document.querySelectorAll('#slideshow .slide-item');
+var slidesLeft = document.querySelectorAll('#slideshow-left .slide-item');
+var slidesRight = document.querySelectorAll('#slideshow-right .slide-item');
 var currentSlide = 0;
 setInterval(nextSlide,5000);
 
 function nextSlide() {
-    slides[currentSlide].className = 'slide-item';
-    currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].className = 'slide-item show';
+    slidesLeft[currentSlide].className = 'slide-item';
+    slidesRight[currentSlide].className = 'slide-item';
+    currentSlide = (currentSlide+1)%slidesLeft.length;
+    slidesLeft[currentSlide].className = 'slide-item show';
+    slidesRight[currentSlide].className = 'slide-item show';
 }
